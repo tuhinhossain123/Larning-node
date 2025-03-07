fc5082454c275b0287e7bbb32f60e2787321b0ee
tuhinhossain123 <csetuhin55@gmail.com>
2025-03-07 23:58:42 +0600

    Node Js  Stream and buffer

---
+++ stream-buffer.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+const { buffer } = require("stream/consumers");
+
+// creating a server using raw node.js
+const server = http.createServer();
+
+// listener
+server.on("request", (req, res) => {
+  if (req.url === "/read-file" && req.method === "GET");
+
+  //   streaming file reading
+  const readablesStream = fs.createReadStream(process.cwd() + "/text/read.txt");
+  readablesStream.on("data", (buffer) => {
+    res.write(buffer);
+  });
+  readablesStream.on("end", () => {
+    res.end("Hello From World");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("server is listening on port 5000");
+});
